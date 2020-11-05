7f3c3364d331cb0229567020ebb67e9db98eb188
Alejandro Durango <lejodurango@gmail.com>
2020-11-04 22:05:41 -0500

    Base Actualizada

---
+++ Modelo/Excepciones/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPS_Mejora_tu_Salud.Modelo.Excepciones
+{
+    class Class1
+    {
+    }
+}
